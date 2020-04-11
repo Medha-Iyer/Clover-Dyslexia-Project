f42c95dd4b3fab506016f577070b3e19ddbf23c2
Siddhanth Iyer <prosiddhanth@gmail.com>
2020-04-10 20:53:29 -0700

    idk

---
+++ app/src/main/java/com/example/clover/VoiceResults.java
@@ -0,0 +1,14 @@
+package com.example.clover;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VoiceResults extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice_results);
+    }
+}
