7eba354d324af7ec677d3c5cd98474361a6748b7
Annie Liu <annieliu2002@gmail.com>
2020-04-13 16:03:18 -0700

    merging spelling w master

--- app/src/main/java/com/example/clover/activities/Spelling.java
+++ app/src/main/java/com/example/clover/activities/Spelling.java
@@ -1,4 +1,4 @@
-package com.example.clover;
+package com.example.clover.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.example.clover.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Spelling extends AppCompatActivity {
