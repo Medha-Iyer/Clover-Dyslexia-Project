5c40198e5e40fff9b82c72682ed1fcfb064163c3
Annie Liu <annieliu2002@gmail.com>
2020-04-23 23:55:02 -0700

    with new tabs for book and notes

--- app/src/main/java/com/example/clover/pojo/LibraryCardItem.java
+++ app/src/main/java/com/example/clover/pojo/LibraryCardItem.java
@@ -2,11 +2,12 @@ package com.example.clover.pojo;
 
 import java.io.Serializable;
 
-public class LibraryCardItem implements Serializable {
+public class LibraryCardItem implements Serializable, Comparable <LibraryCardItem> {
     private String itemTitle;
     private String itemText;
     private int id;
     private boolean state; //true is library, false is archive
+    private int position;
 
     private static final long serialVersionUID = 1L;
 
@@ -14,6 +15,7 @@ public class LibraryCardItem implements Serializable {
         itemTitle = title;
         itemText = text;
         state = true;
+        position = 0;
     }
 
     public LibraryCardItem(){
@@ -35,4 +37,19 @@ public class LibraryCardItem implements Serializable {
     public int getId() { return id; }
 
     public void switchState() { state = !state; }
+
+    public void setState(boolean newS) { state = newS; }
+
+
+    public boolean getState() { return state; }
+
+    public int getPosition() { return position; }
+
+    public void setPosition(int i) { position = i; }
+
+    @Override
+    public int compareTo(LibraryCardItem o) {
+        int comparePosition = ((LibraryCardItem)o).getPosition();
+        return this.getPosition()-comparePosition;
+    }
 }
